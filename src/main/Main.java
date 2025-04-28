b75e94a64a62a00d76f7270d463687fe4b25fc98
Mathiiiiis <132449195+Mathiiiiis@users.noreply.github.com>
2025-04-28 13:55:52 +0200

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import view.AccueilGUI;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         SwingUtilities.invokeLater(() -> new AccueilGUI().setVisible(true));
     }
 }
