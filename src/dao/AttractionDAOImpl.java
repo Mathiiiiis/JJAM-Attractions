80d34e6c0cd61d88e190d4ac5d04e895a03092b5
Mathiiiiis <132449195+Mathiiiiis@users.noreply.github.com>
2025-04-28 13:56:46 +0200

    Update AttractionDAOImpl.java

--- src/dao/AttractionDAOImpl.java
+++ src/dao/AttractionDAOImpl.java
@@ -6,7 +6,7 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List; 
 
-public class AttractionDAOImpl {
+public class AttractionDAOImpl { 
     // Méthode pour récupérer toutes les attractions
     public List<Attraction> getAllAttractions() {
         List<Attraction> list = new ArrayList<>();
