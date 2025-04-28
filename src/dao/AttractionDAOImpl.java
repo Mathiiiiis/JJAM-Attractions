f0acd426f40df6e0cf53dc8382055372ae88044a
Mathiiiiis <132449195+Mathiiiiis@users.noreply.github.com>
2025-04-28 13:57:14 +0200

    YE

--- src/dao/AttractionDAOImpl.java
+++ src/dao/AttractionDAOImpl.java
@@ -10,7 +10,7 @@ public class AttractionDAOImpl {
     // Méthode pour récupérer toutes les attractions
     public List<Attraction> getAllAttractions() {
         List<Attraction> list = new ArrayList<>();
-        String sql = "SELECT * FROM attractions";
+        String sql = "SELECT * FROM attractions";// Recherche dans la BDD
 
         try (Connection conn = DBConnection.getConnection();
              Statement stmt = conn.createStatement();
